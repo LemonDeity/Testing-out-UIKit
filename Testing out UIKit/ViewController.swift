ee16725ce48e33270aa672be404e94fbbdfc1972
Noah R <nrange2001@gmail.com>
2019-10-01 11:11:52 -0500

    Initial Commit

---
+++ Testing out UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing out UIKit
+//
+//  Created by Range, Noah H on 10/1/19.
+//  Copyright © 2019 Range, Noah H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
