72aec26623add55fbc57c6000ee5f5f7ee551625
Noah R <nrange2001@gmail.com>
2019-10-03 12:00:34 -0500

    frst commit

--- Testing out UIKit/ViewController.swift
+++ Testing out UIKit/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var field: UITextField!
+    @IBOutlet weak var Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        field.placeholder = "Enter Text"
     }
 
-
+   
+    @IBAction func ReturnKeyTapped(_ sender: UITextField) {
+        Label.text = field.text
+        if let str = field.text{
+            detectKeyWord(input: str)
+        }
+        field.resignFirstResponder()
+    }
+    
+    func detectKeyWord(input:String){
+        let lower = input.lowercased()
+        if lower.contains("change"){
+            //means we need to make a change
+            colorCheck(lower)
+        }
+    }
+    
+    func colorCheck(_ lower:String){
+        let colors:[String] = ["red","blue","black","green","purple","white","yellow","orange","magenta"]
+        let uColor:[UIColor] = [.red,.blue,.black,.green,.purple,.white,.yellow,.orange,.magenta]
+        if lower.contains("color"){
+            for c in 0...colors.count-1{
+                if lower.contains(colors[c]){
+                    Label.backgroundColor = uColor[c]
+                    break
+                }
+            }
+        }
+    }
+    
 }
 
